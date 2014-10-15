aef011887ad7921f27abad4ba070fb9908fb6eb5
JahazielGuzman <ja.xi.es@hotmail.com>
2014-10-15 14:01:12 -0400

    Update copy_bam.sh

--- copy_bam.sh
+++ copy_bam.sh
@@ -1,4 +1,4 @@
-# use this statement to identify rRNA sequences from the aegypti.gff3 annotation file
+# use these statement to identify rRNA sequences from the aegypti.gff3 annotation file
 filter_rRNA=$(egrep "VectorBase.rRNA" /data/temp_bam/aegypti.gff3 | cut -f 9 |cut -d' ' -f 1 | cut -d'=' -f 2)
 RNAs=$(for RNA in $filter_rRNA; do echo $RNA; done)
 # extra formatting may be necessary, remove semicolons
@@ -16,6 +16,8 @@ bam_dir1="/media/jaxi/differential_expression/stranded"
 bam_dir2="/media/jaxi/differential_expression/unstranded"
 ANNOT="/media/jaxi/differential_expression/noriboaegypti.gff3"
 
+sed -ri "s/ID=/gene_id=/g" $ANNOT 
+
 # get the name of each file on a seperate line for stranded and unstranded
 FILE1=$(ls $bam_dir1 | egrep "accepted")
 FILE2=$(ls $bam_dir2 | egrep "accepted")
@@ -30,8 +32,29 @@ for f1 in $FILE1; do samtools sort -@ 11 -n $bam_dir1/$f1 "sorted."${f1%".bam"};
 
 for f2 in $FILE2; do samtools sort -@ 11 -n $bam_dir2/$f2 "sorted."${f2%".bam"}; done
 
+mv sorted*0* stranded/
+mv sorted* unstranded/
+
+###################################################################################
+############ only run these lines again if you have the files and #################
+############  only want to do read summarization ################
+
+#set the paths where stranded and unstranded bam files will be
+bam_dir1="/media/jaxi/differential_expression/stranded"
+bam_dir2="/media/jaxi/differential_expression/unstranded"
+ANNOT="/media/jaxi/differential_expression/noriboaegypti.gff3"
+
+# get the name of each file on a seperate line for stranded and unstranded
+FILE1=$(ls $bam_dir1 | egrep "accepted")
+FILE2=$(ls $bam_dir2 | egrep "accepted")
+
+#########################################################################
+
 #featureCounts read summarization
 
-for f1 in $FILE1; do featureCounts $bam_dir1/$f1 -a $ANNOT -F -g -f -t 'exon' -O -s 1 -M -T 12 -p -o ${f1%".bam"}".txt"; done
+for f1 in $FILE1; do featureCounts $bam_dir1/"sorted."$f1 -a $ANNOT -F -g -f -t 'exon' -O -s 1 -M -T 12 -p -o "sorted."${f1%".bam"}".txt"; done
 
-for f2 in $FILE2; do featureCounts $bam_dir2/$f2 -a $ANNOT -F -g -f -t 'exon' -O -s 0 -M -T 12 -p -o ${f2%".bam"}".txt"; done
+for f2 in $FILE2; do featureCounts $bam_dir2/"sorted."$f2 -a $ANNOT -F -g -f -t 'exon' -O -s 0 -M -T 12 -p -o "sorted."${f2%".bam"}".txt"; done
+
+mv accepted*0* stranded/diff_expf/
+mv accepted* unstranded/diff_expf/
